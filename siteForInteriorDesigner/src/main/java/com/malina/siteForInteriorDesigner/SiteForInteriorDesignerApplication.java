8123af9e7219a3061c55d87e2485e8ff571b60ef
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2024-04-04 13:40:55 +0300

    add project

---
+++ siteForInteriorDesigner/src/main/java/com/malina/siteForInteriorDesigner/SiteForInteriorDesignerApplication.java
@@ -0,0 +1,13 @@
+package com.malina.siteForInteriorDesigner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteForInteriorDesignerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteForInteriorDesignerApplication.class, args);
+	}
+
+}
---
+++ siteForInteriorDesigner/src/test/java/com/malina/siteForInteriorDesigner/SiteForInteriorDesignerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malina.siteForInteriorDesigner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteForInteriorDesignerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
