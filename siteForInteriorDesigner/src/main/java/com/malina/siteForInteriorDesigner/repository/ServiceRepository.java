8b5d98c4491bc465c20da283caf63b488faf5e8b
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2024-04-18 01:41:30 +0300

    fix logic mistake: ServiceRepository -> ServiceEntity

--- siteForInteriorDesigner/src/main/java/com/malina/siteForInteriorDesigner/repository/ServiceRepository.java
+++ siteForInteriorDesigner/src/main/java/com/malina/siteForInteriorDesigner/repository/ServiceRepository.java
@@ -1,8 +1,9 @@
 package com.malina.siteForInteriorDesigner.repository;
 
+import com.malina.siteForInteriorDesigner.entity.ServiceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ServiceRepository extends JpaRepository<ServiceRepository, Long> {
+public interface ServiceRepository extends JpaRepository<ServiceEntity, Long> {
 }
