3aa3f7fd35295e232ed115db03cf9d57c39096de
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2024-04-18 01:47:40 +0300

    add annotation @Service

--- siteForInteriorDesigner/src/main/java/com/malina/siteForInteriorDesigner/service/impl/ServiceServiceImpl.java
+++ siteForInteriorDesigner/src/main/java/com/malina/siteForInteriorDesigner/service/impl/ServiceServiceImpl.java
@@ -3,10 +3,11 @@ package com.malina.siteForInteriorDesigner.service.impl;
 import com.malina.siteForInteriorDesigner.entity.ServiceEntity;
 import com.malina.siteForInteriorDesigner.repository.ServiceRepository;
 import com.malina.siteForInteriorDesigner.service.ServiceService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
+@Service
 public class ServiceServiceImpl implements ServiceService {
     public final ServiceRepository repository;
 
