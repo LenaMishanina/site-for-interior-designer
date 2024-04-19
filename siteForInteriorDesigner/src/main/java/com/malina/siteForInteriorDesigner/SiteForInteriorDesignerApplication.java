aceb862d4a433d096af8c8daee0e6b322c404f6a
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2024-04-19 19:07:32 +0300

    comment func run -> save image

--- siteForInteriorDesigner/src/main/java/com/malina/siteForInteriorDesigner/SiteForInteriorDesignerApplication.java
+++ siteForInteriorDesigner/src/main/java/com/malina/siteForInteriorDesigner/SiteForInteriorDesignerApplication.java
@@ -14,19 +14,21 @@ import java.nio.file.Paths;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.malina")
-public class SiteForInteriorDesignerApplication implements ApplicationRunner {
-	@Autowired
-	PortfolioRepository portfolioRepository;
+public class SiteForInteriorDesignerApplication
+//		implements ApplicationRunner
+{
+//	@Autowired
+//	PortfolioRepository portfolioRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(SiteForInteriorDesignerApplication.class, args);
 	}
 
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		PortfolioEntity image = new PortfolioEntity();
-		image.setFilename("doctor-who.jpg");
-		image.setMime_type("image/jpeg");
-		image.setData(Files.readAllBytes(Paths.get("doctor-who.jpg")));
-		portfolioRepository.save(image);
-	}
+//	@Override
+//	public void run(ApplicationArguments args) throws Exception {
+//		PortfolioEntity image = new PortfolioEntity();
+//		image.setFilename("doctor-who.jpg");
+//		image.setMime_type("image/jpeg");
+//		image.setData(Files.readAllBytes(Paths.get("doctor-who.jpg")));
+//		portfolioRepository.save(image);
+//	}
 }
