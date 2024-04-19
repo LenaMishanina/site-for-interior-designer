e4af534a6b50ec622495b644fda545a9a14034c8
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2024-04-19 19:29:34 +0300

    impl PortfolioController -> output image

--- siteForInteriorDesigner/src/main/java/com/malina/siteForInteriorDesigner/controller/PortfolioController.java
+++ siteForInteriorDesigner/src/main/java/com/malina/siteForInteriorDesigner/controller/PortfolioController.java
@@ -1,9 +1,29 @@
 package com.malina.siteForInteriorDesigner.controller;
 
+import com.malina.siteForInteriorDesigner.entity.PortfolioEntity;
+import com.malina.siteForInteriorDesigner.service.PortfolioService;
+import org.apache.tomcat.util.file.ConfigurationSource;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/portfolio")
 public class PortfolioController {
+    private final PortfolioService service;
+
+    public PortfolioController(PortfolioService service) {
+        this.service = service;
+    }
+    @GetMapping("/portfolio/{filename}")
+    public ResponseEntity<Resource> retrieve(@PathVariable String filename) {
+        PortfolioEntity image = service.getImage(filename);
+        ByteArrayResource body = new ByteArrayResource(image.getData());
+
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, image.getMime_type()).body(body);
+    }
 }
