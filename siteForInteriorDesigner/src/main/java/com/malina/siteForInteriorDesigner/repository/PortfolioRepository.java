069e518150bf4bf87f2ece37ec5211388c74ae26
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2024-04-19 18:55:40 +0300

    add func findByFilename

--- siteForInteriorDesigner/src/main/java/com/malina/siteForInteriorDesigner/repository/PortfolioRepository.java
+++ siteForInteriorDesigner/src/main/java/com/malina/siteForInteriorDesigner/repository/PortfolioRepository.java
@@ -4,6 +4,9 @@ import com.malina.siteForInteriorDesigner.entity.PortfolioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PortfolioRepository extends JpaRepository<PortfolioEntity, Long> {
+    Optional<PortfolioEntity> findByFilename(String filename);
 }
