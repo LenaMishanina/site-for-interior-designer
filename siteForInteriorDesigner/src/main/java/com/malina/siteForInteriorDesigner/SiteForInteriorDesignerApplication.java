7414e54a5f14a97b1ba2f790d5ff8e47e17db20b
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2024-04-19 18:55:19 +0300

    impl logic - insert into portfolio

--- siteForInteriorDesigner/src/main/java/com/malina/siteForInteriorDesigner/SiteForInteriorDesignerApplication.java
+++ siteForInteriorDesigner/src/main/java/com/malina/siteForInteriorDesigner/SiteForInteriorDesignerApplication.java
@@ -1,15 +1,32 @@
 package com.malina.siteForInteriorDesigner;
 
+import com.malina.siteForInteriorDesigner.entity.PortfolioEntity;
+import com.malina.siteForInteriorDesigner.repository.PortfolioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 @SpringBootApplication
 @ComponentScan(basePackages = "com.malina")
-public class SiteForInteriorDesignerApplication {
-
+public class SiteForInteriorDesignerApplication implements ApplicationRunner {
+	@Autowired
+	PortfolioRepository portfolioRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(SiteForInteriorDesignerApplication.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		PortfolioEntity image = new PortfolioEntity();
+		image.setFilename("doctor-who.jpg");
+		image.setMime_type("image/jpeg");
+		image.setData(Files.readAllBytes(Paths.get("doctor-who.jpg")));
+		portfolioRepository.save(image);
+	}
 }
