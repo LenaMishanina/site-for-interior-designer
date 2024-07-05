2dfbc21a832242671a8429820de302523229d600
Ivan31122003 <Ivan31122003@mail.ru>
2024-07-05 12:58:41 +0300

    Changed config for build version of front-project

--- part_of_code/vite.config.js
+++ part_of_code/vite.config.js
@@ -1,11 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    viteStaticCopy({
+      targets: [
+          {
+              src: 'src/assets/images', // путь к вашей папке
+              dest: 'src/assets' // путь назначения в dist
+          }
+      ]
+  })
   ],
   resolve: {
     alias: {
