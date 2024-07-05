6de4aad048bcc64a6e128bb5f94e0a5d47e9a202
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2024-07-05 21:11:23 +0300

    working config

--- siteForInteriorDesigner/src/main/java/com/malina/siteForInteriorDesigner/config/SecurityConfig.java
+++ siteForInteriorDesigner/src/main/java/com/malina/siteForInteriorDesigner/config/SecurityConfig.java
@@ -1,26 +1,17 @@
 package com.malina.siteForInteriorDesigner.config;
 
-import org.apache.catalina.User;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-//@EnableWebSecurity
 public class SecurityConfig {
-//    public InMemoryUserDetailsManager userDetailsManager() {
-//return null;
-//    }
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         return http
-//                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeRequests(auth -> {
                     auth.requestMatchers("/**").permitAll();
 //                    auth.requestMatchers("/").permitAll();
