9e92fcdd057b146f2ff850bc29e8a381a4f4025e
Ivan31122003 <Ivan31122003@mail.ru>
2024-07-10 19:13:16 +0300

    Changed config

--- backend/src/main/java/com/malina/siteForInteriorDesigner/config/WebConfig.java
+++ backend/src/main/java/com/malina/siteForInteriorDesigner/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Замените на ваш фронтенд домен
+                .allowedOrigins("http://milkovaproject.ru", "https://milkovaproject.ru", "http://localhost:3000", "http://95.163.221.222:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
