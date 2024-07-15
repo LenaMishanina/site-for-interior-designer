478fcbcb8f0bff640deed5c6b4c4e6100a5b06fe
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2024-07-15 16:51:03 +0300

    delete photo -> change indexes

--- siteForInteriorDesigner/src/main/java/com/malina/siteForInteriorDesigner/controller/PortfolioController.java
+++ siteForInteriorDesigner/src/main/java/com/malina/siteForInteriorDesigner/controller/PortfolioController.java
@@ -5,8 +5,15 @@ import com.malina.siteForInteriorDesigner.entity.ServiceEntity;
 import com.malina.siteForInteriorDesigner.service.PortfolioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 @RestController
@@ -17,19 +24,40 @@ public class PortfolioController {
     public PortfolioController(PortfolioService service) {
         this.service = service;
     }
+
+    @PostMapping("/portfolio/uploadPhoto")
+    public String uploadPhoto(@RequestParam("file") MultipartFile file) throws IOException {
+        String fileName = file.getOriginalFilename();
+        System.out.println("FILENAME : " + fileName);
+       // Files.copy(file.getInputStream(), Paths.get("C:\\Users\\misha\\Documents\\GitHub\\site-for-interior-designer\\frontend\\src\\assets\\images\\" + fileName), StandardCopyOption.REPLACE_EXISTING);
+
+        PortfolioEntity photo = new PortfolioEntity();
+        photo.setPath("../src/assets/images/" + fileName);
+        photo.setIndex(service.getLastIndex() + 1);
+        service.addImage(photo);
+
+        return "Photo uploaded successfully";
+    }
+
+
     @GetMapping("/portfolio")
     public List<PortfolioEntity> getPortfolio() {
-        PortfolioEntity img1 = new PortfolioEntity(
-                Long.valueOf(1),
-                "../src/assets/images/complectation.png",
-                0
-        );
-        PortfolioEntity img2 = new PortfolioEntity(
-                Long.valueOf(2),
-                "../src/assets/images/project_frame.png",
-                1
-        );
+//        PortfolioEntity img1 = new PortfolioEntity(
+//                Long.valueOf(1),
+//                "../src/assets/images/complectation.png",
+//                0
+//        );
+//        PortfolioEntity img2 = new PortfolioEntity(
+//                Long.valueOf(2),
+//                "../src/assets/images/project_frame.png",
+//                1
+//        );
 //        return Arrays.asList(img1, img2);
+        List<PortfolioEntity> images = service.findAllImage();
+        images.stream()
+                .sorted(Comparator.comparingInt(PortfolioEntity::getIndex))
+                .forEach(entity -> entity.setIndex(images.indexOf(entity)));
+        service.saveAllImage(images);
         return service.findAllImage();
     }
     @PostMapping("/portfolio/add")
@@ -48,6 +76,12 @@ public class PortfolioController {
     @DeleteMapping("/portfolio/{id}")
     public String deleteImage(@PathVariable("id") long id){
         service.deleteImage(id);
+
+        //Обновляет index после удаления фото
+//        List<PortfolioEntity> images = new ArrayList<>();
+//        images.stream()
+//                .sorted(Comparator.comparingInt(PortfolioEntity::getIndex))
+//                .forEach(entity -> entity.setIndex(images.indexOf(entity)));
         return "Image was removed successfully...";
     }
 
