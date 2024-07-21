431ed3fd9fad59e27439c77606834a133222536a
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2024-07-21 16:32:21 +0300

    remove validation

--- frontend/src/views/AddService.vue
+++ frontend/src/views/AddService.vue
@@ -8,32 +8,32 @@
                <!--name-->
                 <div class="name">
                   <label for="name" class="form_label">Название:</label>
-                  <input id="name"  type="text" name="name" class="form-control" required v-model="service.name">
+                  <input id="name"  type="text" name="name" class="form-control" v-model="service.name">
                 </div>
 
               
               <!--short_description-->
                   <div class="short_description">
                     <label for="short_description" class="form_label">Видимое описание:</label>
-                    <input id="short_description" type="text"  name="short_description" class="form-control" required v-model="service.short_description" >
+                    <input id="short_description" type="text"  name="short_description" class="form-control" v-model="service.short_description" >
                   </div>
 
               <!--long_description-->
                   <div class="long_description">
                     <label for="long_description" class="form_label">Скрытое описание:</label>
-                    <input id="long_description" type="text"  name="long_description" class="form-control" required v-model="service.long_description" >
+                    <input id="long_description" type="text"  name="long_description" class="form-control" v-model="service.long_description" >
                   </div>
     
               <!--price-->
                   <div class="price">
                     <label for="price" class="form_label">Стоимость:</label>
-                    <input id="price" type="number"  name="price" class="form-control" required v-model="service.price" >
+                    <input id="price" type="number"  name="price" class="form-control" v-model="service.price" >
                   </div>
 
               <!--value_measure-->
                   <div class="value_measure">
                     <label for="value_measure" class="form_label">Ед. измерения стоимости:</label>
-                    <input id="value_measure" type="text"  name="value_measure" class="form-control" required v-model="service.value_measure" >
+                    <input id="value_measure" type="text"  name="value_measure" class="form-control" v-model="service.value_measure" >
                   </div>
 
                 <div>
